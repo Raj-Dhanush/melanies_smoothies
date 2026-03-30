e90065eee3b296e79c69e30f41af03355c490195
Raj-Dhanush <rajdhanushmanukonda@gmail.com>
2026-03-30 12:44:03 +0530

    Replace Snowflake session retrieval method

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
 import pandas as pd
@@ -13,8 +12,8 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-# Snowflake session
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # ✅ Include SEARCH_ON column
 my_dataframe = session.table("smoothies.public.fruit_options") \

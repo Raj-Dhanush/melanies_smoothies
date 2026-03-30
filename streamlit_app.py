391e497fe5e58372dfaf35c9a5926f739fea7940
Raj-Dhanush <rajdhanushmanukonda@gmail.com>
2026-03-30 12:33:53 +0530

    Add subheader for fruit nutrition information
    Added subheader for displaying nutrition information for each chosen fruit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,12 +31,14 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        # API call for each fruit
+        # 🔥 NEW (as per tutorial)
+        st.subheader(fruit_chosen + ' Nutrition Information')
+
         smoothiefroot_response = requests.get(
-            f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen.lower()}"
+            "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
         )
 
-        # Display dataframe
+        # Display dataframe (even if error comes)
         st.dataframe(
             data=smoothiefroot_response.json(),
             use_container_width=True

2ed8bf11b0c7b7e990d1cbda78aa5dd46c08549c
Raj-Dhanush <rajdhanushmanukonda@gmail.com>
2026-03-30 11:39:50 +0530

    Create streamlit_app.py
    copied from my Custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,47 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Title
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+
+st.write("Choose the fruits you want in your custom Smoothie!")
+
+# ✅ Name input
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
+
+# Snowflake session
+session = get_active_session()
+
+# Fetch only FRUIT_NAME column
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+
+# Multiselect
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    my_dataframe
+)
+
+# IF block
+if ingredients_list:
+    ingredients_string = ''
+
+    # FOR loop (as required by lab)
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+    # ✅ Build SQL (2 columns)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                        values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+
+    # Submit button
+    time_to_insert = st.button('Submit Order')
+
+    # Insert only when button clicked
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+
+        # Success message with name
+        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
